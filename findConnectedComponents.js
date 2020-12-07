c10fe22237881b4dc061513a7ae4e5ecc71c7d0f
Biswajitghosh98 <Biswajitghosh98@users.noreply.github.com>
2020-12-07 15:59:04 +0000

    Prettified Code!

--- findConnectedComponents.js
+++ findConnectedComponents.js
@@ -1,99 +1,88 @@
-const GraphBase = require('./graph-base')
-const Stack = require('./ds/stack')
+const GraphBase = require('./graph-base');
+const Stack = require('./ds/stack');
 
-class FindConnectedComponents extends GraphBase{
+class FindConnectedComponents extends GraphBase {
+  constructor(numVertices = 0, numEdges = 0) {
+    super(numVertices, numEdges);
+  }
 
-    constructor(numVertices = 0, numEdges = 0){
-        super(numVertices, numEdges)
-    }
+  findConnectedComponents() {
+    let vertices = [];
+    let visited = [];
+    let all_connected_components = [];
 
-    findConnectedComponents()
-    {
-        let vertices = []
-        let visited = []
-        let all_connected_components = []
+    let Adj = this.getSimpleAdj();
 
-        let Adj = this.getSimpleAdj()
+    Adj.forEach(function (value, key) {
+      vertices.push(key);
+      visited[key] = false;
+    });
 
-        Adj.forEach(function(value,key){
-            vertices.push(key) 
-            visited[key] = false     
-        })
+    for (let i = 0; i < vertices.length; i++) {
+      let stack = new Stack();
+      let node = vertices[i];
+      if (visited[node]) continue;
 
-        for(let i=0;i<vertices.length;i++)
-        {
-            let stack = new Stack()
-            let node = vertices[i]
-            if(visited[node])
-                continue;
+      stack.push(node);
+      let current_connected_components = [node];
 
-            stack.push(node)
-            let current_connected_components = [node] 
+      visited[node] = true;
 
-            visited[node] = true
+      while (!stack.isEmpty()) {
+        let current_node = stack.pop();
+        let neighbours = Adj.get(current_node);
 
-            while(!stack.isEmpty())
-            {
-                let current_node = stack.pop()
-                let neighbours = Adj.get(current_node)
+        for (let j = 0; j < neighbours.length; j++) {
+          let neighbouring_node = neighbours[j];
 
-                for(let j=0;j<neighbours.length;j++)
-                {
-                    let neighbouring_node = neighbours[j]
-
-                    if(!visited[neighbouring_node])
-                    {
-                        visited[neighbouring_node] = true
-                        current_connected_components.push(neighbouring_node)
-                        stack.push(neighbouring_node)
-                    }
-                }
-                
-            }
-
-            all_connected_components.push(current_connected_components)
-
+          if (!visited[neighbouring_node]) {
+            visited[neighbouring_node] = true;
+            current_connected_components.push(neighbouring_node);
+            stack.push(neighbouring_node);
+          }
         }
+      }
 
-        return all_connected_components
-        //returns list of lists 
+      all_connected_components.push(current_connected_components);
     }
+
+    return all_connected_components;
+    //returns list of lists
+  }
 }
 
 /**********   TEST RUN **********************/
 
-const input_file = require('./testfiles/test2.json')
-const fs = require("fs")
-output_data = new Map()
+const input_file = require('./testfiles/test2.json');
+const fs = require('fs');
+output_data = new Map();
 
-for(let testcase=0;testcase<input_file.length;testcase++)
-{
-    let input = input_file[testcase]
+for (let testcase = 0; testcase < input_file.length; testcase++) {
+  let input = input_file[testcase];
 
-    const g = new FindConnectedComponents(input.numVertices)
+  const g = new FindConnectedComponents(input.numVertices);
 
-    vertices = input.vertices
+  vertices = input.vertices;
 
-    for(let i=0;i<vertices.length;i++)
-    {
-        g.addVertex(vertices[i])
-    }
+  for (let i = 0; i < vertices.length; i++) {
+    g.addVertex(vertices[i]);
+  }
 
-    for(let i=0;i<input.numEdges;i++)
-    {
-        let edge = input.edges[i]
-        g.addEdge(edge[0],edge[1])
-        g.addEdge(edge[1],edge[0])
-    }
+  for (let i = 0; i < input.numEdges; i++) {
+    let edge = input.edges[i];
+    g.addEdge(edge[0], edge[1]);
+    g.addEdge(edge[1], edge[0]);
+  }
 
-    // console.log("Output For TestCase " + (testcase+1)+ " :")
-    // console.log(g.findConnectedComponents())
+  // console.log("Output For TestCase " + (testcase+1)+ " :")
+  // console.log(g.findConnectedComponents())
 
-    output_data.set("Testcase "+ testcase , g.findConnectedComponents())
-
+  output_data.set('Testcase ' + testcase, g.findConnectedComponents());
 }
 
-fs.writeFileSync("testfiles/output.json",JSON.stringify(Array.from(output_data)))
+fs.writeFileSync(
+  'testfiles/output.json',
+  JSON.stringify(Array.from(output_data))
+);
 
-
-module.exports = FindConnectedComponents
+module.exports = FindConnectedComponents;
--- toposort_cycle_find.js
+++ toposort_cycle_find.js
@@ -1,187 +1,158 @@
-const GraphBase = require('./graph-base')
-const Queue = require('./ds/queue')
+const GraphBase = require('./graph-base');
+const Queue = require('./ds/queue');
 
-class FindCycle extends GraphBase{
-    
-    constructor(numVertices = 0, numEdges = 0, isDirected = false){
-        super(numVertices, numEdges)
-        this.isDirected = isDirected
+class FindCycle extends GraphBase {
+  constructor(numVertices = 0, numEdges = 0, isDirected = false) {
+    super(numVertices, numEdges);
+    this.isDirected = isDirected;
+  }
+
+  findCycle() {
+    let isCyclic = false;
+
+    let Adj = this.getSimpleAdj();
+
+    if (this.isDirected) {
+      let sorted_list = this.toposort();
+      let pos = [];
+
+      for (let i = 0; i < sorted_list.length; i++) {
+        let node = sorted_list[i];
+        pos[node] = i;
+      }
+
+      Adj.forEach(function (children, parent) {
+        for (let i = 0; i < children.length; i++) {
+          let child = children[i];
+          if (pos[parent] > pos[child]) {
+            isCyclic = true;
+          }
+          if (isCyclic) break;
+        }
+      });
+    } else {
+      let visited = [],
+        vertices = [],
+        parent = [];
+
+      Adj.forEach(function (neighbours, vertex) {
+        visited[vertex] = false;
+        parent[vertex] = -1;
+        vertices.push(vertex);
+      });
+
+      for (let i = 0; i < vertices.length; i++) {
+        let current_vertex = vertices[i];
+
+        if (visited[current_vertex]) continue;
+
+        let queue = new Queue();
+        visited[current_vertex] = true;
+        queue.enqueue(current_vertex);
+
+        while (!queue.isEmpty()) {
+          let vertex = queue.dequeue();
+
+          for (let i = 0; i < Adj.get(vertex).length; i++) {
+            let next_vertex = Adj.get(vertex)[i];
+            if (!visited[next_vertex]) {
+              visited[next_vertex] = true;
+              queue.enqueue(next_vertex);
+              parent[next_vertex] = vertex;
+            } else if (parent[vertex] != next_vertex) {
+              isCyclic = true;
+              break;
+            }
+          }
+
+          if (isCyclic) break;
+        }
+
+        if (isCyclic) break;
+      }
     }
 
-    findCycle()
-    {
-        let isCyclic = false
+    return isCyclic;
+  }
 
-        let Adj = this.getSimpleAdj()
+  toposort() {
+    let Adj = this.getSimpleAdj();
+    let visited = [];
+    let vertices = [];
 
-        if (this.isDirected)
-        {
-            let sorted_list = this.toposort()
-            let pos = []
+    Adj.forEach(function (neighbours, vertex) {
+      visited[vertex] = false;
+      vertices.push(vertex);
+    });
 
-            for(let i=0;i<sorted_list.length;i++)
-            {
-                let node = sorted_list[i]
-                pos[node] = i
-            }
-            
-            Adj.forEach(function(children,parent){
-                for(let i=0;i<children.length;i++)
-                {
-                    let child = children[i]
-                    if(pos[parent]>pos[child])
-                    {
-                        isCyclic = true
-                    }
-                    if(isCyclic)
-                        break
-                }
-            })
+    let topo_stack = [];
 
-        }
-        else
-        {
-            let visited = [], vertices = [], parent = []
+    for (let i = 0; i < vertices.length; i++) {
+      let current_vertex = vertices[i];
 
-            Adj.forEach(function(neighbours,vertex){
-                visited[vertex] = false
-                parent[vertex] = -1
-                vertices.push(vertex)
-            })
-
-            for(let i=0;i<vertices.length;i++)
-            {
-                let current_vertex = vertices[i]
-
-                if(visited[current_vertex])
-                    continue;
-
-                let queue = new Queue()
-                visited[current_vertex] = true
-                queue.enqueue(current_vertex)
-
-                while(!queue.isEmpty())
-                {
-                    let vertex = queue.dequeue()
-
-                    for(let i=0;i<Adj.get(vertex).length;i++)
-                    {
-                        let next_vertex = Adj.get(vertex)[i]
-                        if(!visited[next_vertex])
-                        {
-                            visited[next_vertex] = true
-                            queue.enqueue(next_vertex)
-                            parent[next_vertex] = vertex
-                        }
-                        else if(parent[vertex] != next_vertex)
-                        {
-                            isCyclic = true
-                            break
-                        }
-                    }
-
-                    if(isCyclic)
-                        break
-                }
-
-                if(isCyclic)
-                    break
-            }
-
-        }
-
-        return isCyclic
+      if (!visited[current_vertex]) {
+        dfs_recursive(Adj, current_vertex, visited, topo_stack);
+      }
     }
 
-    toposort()
-    {
-        let Adj = this.getSimpleAdj()
-        let visited = []
-        let vertices = []
+    let sorted_list = [];
+    while (topo_stack.length) {
+      sorted_list.push(topo_stack.pop());
+    }
 
-        Adj.forEach(function(neighbours,vertex){
-            visited[vertex] = false
-            vertices.push(vertex)
-        })
-
-        let topo_stack = []
-
-        for(let i=0;i<vertices.length;i++)
-        {
-            let current_vertex = vertices[i]
-
-            if(!visited[current_vertex])
-            {
-                dfs_recursive(Adj,current_vertex,visited,topo_stack)
-            }
-        }
-
-        let sorted_list = []
-        while(topo_stack.length)
-        {
-            sorted_list.push(topo_stack.pop())
-        }
-
-        return sorted_list 
-
-    }    
+    return sorted_list;
+  }
 }
 
+function dfs_recursive(Adj, current_vertex, visited, topo_stack = []) {
+  visited[current_vertex] = true;
 
-function dfs_recursive(Adj, current_vertex, visited, topo_stack = [])
-{
-    visited[current_vertex] = true
-
-    for(let i = 0;i<Adj.get(current_vertex).length;i++)
-    {
-        let next_vertex = Adj.get(current_vertex)[i]
-        if(!visited[next_vertex])
-        {
-            dfs_recursive(Adj,next_vertex,visited,topo_stack)
-        }
+  for (let i = 0; i < Adj.get(current_vertex).length; i++) {
+    let next_vertex = Adj.get(current_vertex)[i];
+    if (!visited[next_vertex]) {
+      dfs_recursive(Adj, next_vertex, visited, topo_stack);
     }
+  }
 
-    topo_stack.push(current_vertex)
+  topo_stack.push(current_vertex);
 }
-
 
 /*****************   TEST RUN **********************/
 
-const input_file = require('./testfiles/test1.json')
-const fs = require("fs")
-output_data = new Map()
+const input_file = require('./testfiles/test1.json');
+const fs = require('fs');
+output_data = new Map();
 
-for(let testcase=0;testcase<input_file.length;testcase++)
-{
-    let input = input_file[testcase]
+for (let testcase = 0; testcase < input_file.length; testcase++) {
+  let input = input_file[testcase];
 
-    const g = new FindCycle(input.numVertices,input.numEdges,input.isDirected)
+  const g = new FindCycle(input.numVertices, input.numEdges, input.isDirected);
 
-    vertices = input.vertices
+  vertices = input.vertices;
 
-    for(let i=0;i<vertices.length;i++)
-    {
-        g.addVertex(vertices[i])
+  for (let i = 0; i < vertices.length; i++) {
+    g.addVertex(vertices[i]);
+  }
+
+  for (let i = 0; i < input.numEdges; i++) {
+    let edge = input.edges[i];
+    g.addEdge(edge[0], edge[1]);
+    if (input.isDirected == false) {
+      g.addEdge(edge[1], edge[0]);
+      //only add when undirected.
     }
+  }
 
-    for(let i=0;i<input.numEdges;i++)
-    {
-        let edge = input.edges[i]
-        g.addEdge(edge[0],edge[1])
-        if(input.isDirected == false)
-        {
-            g.addEdge(edge[1],edge[0])
-            //only add when undirected.
-        }
-    }
+  // console.log("Output For TestCase " + (testcase+1)+ " : g.findCycle()")
 
-    // console.log("Output For TestCase " + (testcase+1)+ " : g.findCycle()")
-
-    output_data.set("Testcase "+ testcase , g.findCycle())
+  output_data.set('Testcase ' + testcase, g.findCycle());
 }
 
 // console.log(output_data)
 
-fs.writeFileSync("testfiles/output.json",JSON.stringify(Array.from(output_data)))
+fs.writeFileSync(
+  'testfiles/output.json',
+  JSON.stringify(Array.from(output_data))
+);
 
-module.exports = FindCycle
+module.exports = FindCycle;

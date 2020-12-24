368e88dcfb4a8254682902d4cc6e3d7249542f63
Biswajitghosh98 <Biswajitghosh98@users.noreply.github.com>
2020-12-24 13:26:36 +0000

    Prettified Code!

--- kruskal.js
+++ kruskal.js
@@ -1,62 +1,55 @@
 const GraphBase = require('./graph-base');
-const DSU = require('./ds/dsu')
+const DSU = require('./ds/dsu');
 
-class Kruskal extends GraphBase{
-    constructor(numVertices = 0, numEdges = 0 ,edges){
-        super(numVertices, numEdges)
-        this.edges = edges
-    }
+class Kruskal extends GraphBase {
+  constructor(numVertices = 0, numEdges = 0, edges) {
+    super(numVertices, numEdges);
+    this.edges = edges;
+  }
 
-    sort_edges()
-    {
-        let edges = this.edges
+  sort_edges() {
+    let edges = this.edges;
 
-        for(let i=0;i<edges.length;i++ )
-        {
-            for(let j=i+1;j<edges.length;j++)
-            {
-                if(edges[i][2]>edges[j][2])
-                {
-                    let temp = edges[i]
-                    edges[i] = edges[j]
-                    edges[j] = temp
-                }
-            }
+    for (let i = 0; i < edges.length; i++) {
+      for (let j = i + 1; j < edges.length; j++) {
+        if (edges[i][2] > edges[j][2]) {
+          let temp = edges[i];
+          edges[i] = edges[j];
+          edges[j] = temp;
         }
-        return edges
+      }
+    }
+    return edges;
+  }
+
+  kruskal() {
+    let minSpanningTree = { edgeset: [], total_weight: 0 };
+
+    let sorted_edges = this.sort_edges();
+
+    let vertices = [];
+    let Adj = this.getSimpleAdj();
+
+    Adj.forEach(function (value, key) {
+      vertices.push(key);
+    });
+
+    let disjoint_set = new DSU(vertices);
+
+    for (let i = 0; i < sorted_edges.length; i++) {
+      let edge = sorted_edges[i];
+      let node1 = edge[0],
+        node2 = edge[1];
+      let current_weight = edge[2];
+      if (disjoint_set.find(node1) != disjoint_set.find(node2)) {
+        disjoint_set.union(node1, node2);
+        minSpanningTree.edgeset.push([node1, node2]);
+        minSpanningTree.total_weight += current_weight;
+      }
     }
 
-    kruskal()
-    {
-        let minSpanningTree = { "edgeset" : [] , "total_weight": 0 }
-
-        let sorted_edges = this.sort_edges()
-
-        let vertices = []
-        let Adj = this.getSimpleAdj()
-
-        Adj.forEach(function(value,key){
-            vertices.push(key)
-        })
-
-        let disjoint_set = new DSU(vertices)
-
-        for(let i=0;i<sorted_edges.length;i++)
-        {
-            let edge = sorted_edges[i];
-            let node1 = edge[0] , node2 = edge[1]      
-            let current_weight = edge[2]      
-            if(disjoint_set.find(node1)!=disjoint_set.find(node2))
-            {
-                disjoint_set.union(node1,node2)
-                minSpanningTree.edgeset.push([node1,node2])
-                minSpanningTree.total_weight += current_weight
-            }
-        }       
-        
-        return minSpanningTree 
-
-    }
+    return minSpanningTree;
+  }
 }
 
 /******************************* TEST ************************/
@@ -67,7 +60,7 @@ class Kruskal extends GraphBase{
 
 // for (let testcase = 0; testcase < input_file.length; testcase++) {
 //     let input = input_file[testcase];
-    
+
 //     let vertices = input.vertices
 //     let edges = input.edges
 
@@ -76,7 +69,7 @@ class Kruskal extends GraphBase{
 //     for (let i = 0; i < vertices.length; i++) {
 //         g.addVertex(vertices[i]);
 //       }
-    
+
 //     for (let i = 0; i < input.numEdges; i++) {
 //         let edge = input.edges[i];
 //         g.addEdge(edge[0], edge[1], edge[2]);
--- prim.js
+++ prim.js
@@ -1,108 +1,104 @@
 const GraphBase = require('./graph-base');
 
-const INFINITY = 1e18
+const INFINITY = 1e18;
 
-class PrimsMST extends GraphBase{
+class PrimsMST extends GraphBase {
+  constructor(numVertices = 0, numEgdes = 0, edges) {
+    super(numVertices, numEgdes);
+    this.edges = edges;
+  }
 
-    constructor(numVertices = 0, numEgdes = 0 ,edges)
-    {
-        super(numVertices,numEgdes)
-        this.edges = edges
+  nearest_vertex(vertices, distance, is_taken) {
+    let min_distance = INFINITY;
+    let nearest = -1;
+    for (let i = 0; i < vertices.length; i++) {
+      let v = vertices[i];
+      if (is_taken[v] == false && distance[v] < min_distance) {
+        min_distance = distance[v];
+        nearest = v;
+      }
     }
 
-    nearest_vertex(vertices, distance, is_taken)
-    {
-        let min_distance = INFINITY
-        let nearest = -1
-        for(let i=0;i<vertices.length;i++)
-        {
-            let v = vertices[i]
-            if(is_taken[v]==false && distance[v] < min_distance)
-            {
-                min_distance = distance[v]
-                nearest = v
-            }
-        }
+    return nearest;
+  }
 
-        return nearest
+  getAdjMatrix() {
+    let Adj = this.getSimpleAdj();
+    let vertices = [];
+
+    Adj.forEach(function (value, key) {
+      vertices.push(key);
+    });
+
+    let n = vertices[vertices.length - 1];
+
+    let matrix = new Array(n + 1);
+
+    for (let i = 0; i <= n; i++) {
+      matrix[i] = new Array(n + 1);
+      for (let j = 0; j <= n; j++) {
+        matrix[i][j] = INFINITY;
+      }
+      matrix[i][i] = 0;
     }
 
-    getAdjMatrix()
-    {
-        let Adj = this.getSimpleAdj()
-        let vertices = []
-
-        Adj.forEach(function(value,key){
-            vertices.push(key)
-        })
-
-        let n = vertices[vertices.length-1]
-
-        let matrix = new Array(n+1)
-
-        for(let i=0;i<=n;i++)
-        {
-            matrix[i] = new Array(n+1)
-            for(let j=0;j<=n;j++)
-            {
-                matrix[i][j] = INFINITY
-            }
-            matrix[i][i] = 0 
-        }
-
-        for(let i=0;i<this.edges.length;i++)
-        {
-            let edge = this.edges[i]
-            matrix[edge[0]][edge[1]] = edge[2]
-            matrix[edge[1]][edge[0]] = edge[2]
-        }
-
-        return matrix
+    for (let i = 0; i < this.edges.length; i++) {
+      let edge = this.edges[i];
+      matrix[edge[0]][edge[1]] = edge[2];
+      matrix[edge[1]][edge[0]] = edge[2];
     }
 
-    prims_mst()
-    {
-        let minSpanningTree = { "edgeset" : [], "total_weight" : 0}
-        let Adj = this.getSimpleAdj()
-        let vertices = [],distance = []
-        let parent = [], is_taken = []
+    return matrix;
+  }
 
-        Adj.forEach(function(value,key){
-            vertices.push(key)
-            distance[key] = INFINITY
-            is_taken[key] = false
-            parent[key] = -1
-        })
+  prims_mst() {
+    let minSpanningTree = { edgeset: [], total_weight: 0 };
+    let Adj = this.getSimpleAdj();
+    let vertices = [],
+      distance = [];
+    let parent = [],
+      is_taken = [];
 
-        let n = vertices.length
-        let Adj_Matrix = this.getAdjMatrix()
+    Adj.forEach(function (value, key) {
+      vertices.push(key);
+      distance[key] = INFINITY;
+      is_taken[key] = false;
+      parent[key] = -1;
+    });
 
-        distance[vertices[0]] = 0 //Pick the first vertex always
+    let n = vertices.length;
+    let Adj_Matrix = this.getAdjMatrix();
 
-        for(let i=1;i<=n-1;i++) //Add n-1 edges [n= number of vertices]
-        {
-            let u = this.nearest_vertex(vertices, distance, is_taken)
-            is_taken[u] = true
+    distance[vertices[0]] = 0; //Pick the first vertex always
 
-            for(let j=0;j<n;j++)
-            {
-                let v = vertices[j]
-                if(Adj_Matrix[u][v] > 0 && is_taken[v]==false && Adj_Matrix[u][v] < distance[v] )
-                {
-                    distance[v] = Adj_Matrix[u][v]
-                    parent[v] = u
-                }
-            }
+    for (
+      let i = 1;
+      i <= n - 1;
+      i++ //Add n-1 edges [n= number of vertices]
+    ) {
+      let u = this.nearest_vertex(vertices, distance, is_taken);
+      is_taken[u] = true;
+
+      for (let j = 0; j < n; j++) {
+        let v = vertices[j];
+        if (
+          Adj_Matrix[u][v] > 0 &&
+          is_taken[v] == false &&
+          Adj_Matrix[u][v] < distance[v]
+        ) {
+          distance[v] = Adj_Matrix[u][v];
+          parent[v] = u;
         }
-
-        for(let i=vertices[1];i<=vertices[n-1];i++)
-        {
-            minSpanningTree.edgeset.push([parent[i],i])
-            minSpanningTree.total_weight += Adj_Matrix[parent[i]][i]
-        }
-        
-        return minSpanningTree
+      }
     }
+
+    for (let i = vertices[1]; i <= vertices[n - 1]; i++) {
+      minSpanningTree.edgeset.push([parent[i], i]);
+      minSpanningTree.total_weight += Adj_Matrix[parent[i]][i];
+    }
+
+    return minSpanningTree;
+  }
 }
 
 /*************************TEST ********************************/
@@ -113,7 +109,7 @@ class PrimsMST extends GraphBase{
 
 // for (let testcase = 0; testcase < input_file.length; testcase++) {
 //     let input = input_file[testcase];
-    
+
 //     let vertices = input.vertices
 //     let edges = input.edges
 
@@ -122,7 +118,7 @@ class PrimsMST extends GraphBase{
 //     for (let i = 0; i < vertices.length; i++) {
 //         g.addVertex(vertices[i]);
 //       }
-    
+
 //     for (let i = 0; i < input.numEdges; i++) {
 //         let edge = input.edges[i];
 //         g.addEdge(edge[0], edge[1], edge[2]);

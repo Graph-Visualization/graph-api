d2078dbaf5f3dac5ba1718f255bdf54f180ece78
Devjit_Choudhury <61665451+devartstar@users.noreply.github.com>
2021-01-09 22:14:31 +0530

    Had uploaded the wrong file initially
    This is the code for finding all bridges in an undirected graph

--- bridges.js
+++ bridges.js
@@ -1,43 +1,50 @@
 const GraphBase = require('./graph-base');
+const Queue = require('./ds/queue');
 
-class ArticulationPoint extends GraphBase {
-    constructor(numVertices = 0, numEdges = 0, startingPoint = 0) {
-        super(numVertices, numEdges);
+class FindingBridges extends GraphBase {
+    constructor(numVertices = 0, numEdges = 0,res=[]) {
+        super(numVertices, numEdges,res);
     }
-
-    bridgeUtil(graph, v, time) {
-        graph[v].visited = true;
+    
+    bridgeCheck(u,graph,time){
+        graph[u].visited=true;
         time++;
-        graph[v].disc = time;
-        graph[v].low = time;
-        var child = 0;
-        for (var i = 0; i < graph[v].al.length; i++) {
-            var av = graph[v].al[i];
-            if (graph[av].visited == false) {
-                // child++;
-                graph[av].parent = v;
-                this.bridgeUtil(graph, av, time);
-                graph[v].low = Math.min(graph[v].low, graph[av].low);
-                if (graph[av].low >= graph[v].disc) {
-                    console.log(v+"---"+av);
-                    graph[v].res = true;
+        graph[u].disc=time;
+        graph[u].low=time;
+
+        for(var i=0;i<graph[u].al.length;i++){
+            var av = graph[u].al[i];
+            if(graph[av].visited == false){
+                graph[av].parent = u;
+                this.bridgeCheck(av,graph,time);
+                graph[u].low = Math.min(graph[u].low,graph[av].low);
+
+                if(graph[av].low > graph[u].disc){
+                    graph[u].ans.push(av);
+                    graph[av].ans.push(u);
                 }
-            } else if (av != graph[v].parent) {
-                graph[v].low = Math.min(graph[v].low, graph[av].disc);
+            }
+            else if( av != graph[u].parent){
+                graph[u].low = Math.min(graph[u].low , graph[av].disc);
             }
         }
+
+        return time;
     }
 
     bridge(graph){
-        for(var i=0;i<this.numVertices;i++){
-            if(graph[i].visited == false){
-                this.bridgeUtil(graph,i,0);
+        var t=0;
+        for(var i =0;i<this.numVertices;i++){
+            if(graph[i].visited==false){
+                t=this.bridgeCheck(i,graph,0);
             }
         }
     }
 
-    ArticulationPointCheck() {
+    FindingBridgesCheck() {
         let Adj = this.getSimpleAdj();
+
+
         var graph = [];
         for (var i = 0; i < this.numVertices; i++) {
 
@@ -47,54 +54,57 @@ class ArticulationPoint extends GraphBase {
                 disc: 0,
                 low: 0,
                 parent: -1,
-                res: false
+                ans:[]
             }
         }
 
         // building the array of objects
         Adj.forEach((value, key) => {
             value.forEach((i) => {
-                if (!graph[key].al.includes(i)) {
+                 if (!graph[key].al.includes(i)) {
                 graph[key].al.push(i);
-                }
-                if (!graph[i].al.includes(key)) {
+                 }
+                 if (!graph[i].al.includes(key)) {
                 graph[i].al.push(key);
-                }
+                 }
             });
         });
-
         this.bridge(graph);
 
-
-        var res = [];
-        for (var i = 0; i < this.numVertices; i++) {
-            if (graph[i].res) {
-                res.push(i);
+        var res=[];
+        for(var i=0;i<this.numVertices;i++){
+            for(var j=0;j<graph[i].ans.length;j++){
+                var x = i;
+                var y = graph[i].ans[j];
+                if(x<y){
+                    res.push([x,y]);
+                }
             }
         }
+
         return res;
     }
 }
 
 
-// ----------Ex. of not ArticulationPoint graph
-// const g = new ArticulationPoint(7);
-// vertices = [0, 1, 2, 3,4 ,5,6]
-// for (let i = 0; i < vertices.length; i++) {
-//     g.addVertex(vertices[i]);
-// }
-// g.addEdge(0, 1);
-// g.addEdge(0, 3);
-// g.addEdge(1, 2);
-// g.addEdge(2, 3);
-// g.addEdge(2, 6);
-// g.addEdge(3, 4);
-// g.addEdge(3, 5);
-// g.addEdge(4, 5);
-// console.log(g.ArticulationPointCheck());
+// ----------Ex. of not FindingBridges graph
+//   const g = new FindingBridges(7);
+//   vertices = [0, 1, 2, 3,4 ,5,6]
+//   for (let i = 0; i < vertices.length; i++) {
+//       g.addVertex(vertices[i]);
+//   }
+//   g.addEdge(0, 1); 
+//   g.addEdge(1, 2); 
+//   g.addEdge(2, 0); 
+//   g.addEdge(1, 3); 
+//   g.addEdge(1, 4); 
+//   g.addEdge(1, 6); 
+//   g.addEdge(3, 5); 
+//   g.addEdge(4, 5); 
+//   console.log(g.FindingBridgesCheck());
 
-// ----------Ex. of a ArticulationPoint graph
-//  const f = new ArticulationPoint(5);
+// ----------Ex. of a FindingBridges graph
+//  const f = new FindingBridges(5);
 //  vertices = [0, 1, 2, 3]
 //  for (let i = 0; i < vertices.length; i++) {
 //      f.addVertex(vertices[i]);
@@ -104,6 +114,6 @@ class ArticulationPoint extends GraphBase {
 //  f.addEdge(2,1);
 //  f.addEdge(0,3);
 //  f.addEdge(3,4);
-//  console.log(f.ArticulationPointCheck());
+//  console.log(f.FindingBridgesCheck());
 
-module.exports = ArticulationPoint;
+module.exports = FindingBridges;
